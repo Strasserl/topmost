ec0d7c2b6792ae52eae8ae836095aa18e277fdb5
Lindsay Strasser <lindsay.strasser@gmail.com>
2019-04-25 16:51:02 -0400

    removes React boilerplate homepage

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+// app.get('/api/hello', (req, res) => {
+//   res.send({ express: 'Hello From Express' });
+// });
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
+// app.post('/api/world', (req, res) => {
+//   console.log(req.body);
+//   res.send(
+//     `I received your POST request. This is what you sent me: ${req.body.post}`
+//   );
+// });
 
 app.use('/api', require('./server/api'));
 
