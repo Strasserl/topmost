86f555294a0e1de5be698c9f349b329184b4c032
Lindsay Strasser <lindsay.strasser@gmail.com>
2019-04-25 17:46:51 -0400

    comments out even more code for potential future use

--- client/routes.js
+++ client/routes.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import { withRouter, Route, Switch } from 'react-router-dom';
 import { TeacherHome } from './components';
-import { me } from './store';
+// import { me } from './store';
 
 class Routes extends Component {
   componentDidMount() {
@@ -10,36 +10,39 @@ class Routes extends Component {
   }
 
   render() {
-    const { isLoggedIn } = this.props;
+    // const { isLoggedIn } = this.props;
 
     return (
       <Switch>
-        {isLoggedIn && (
-          <Switch>
-            <Route path="/teacher-home" component={TeacherHome} />
-          </Switch>
-        )}
+        {/* {isLoggedIn && ( */}
+        <Switch>
+          <Route path="/teacher-home" component={TeacherHome} />
+        </Switch>
+        {/* )} */}
       </Switch>
     );
   }
 }
 
-const mapState = state => {
-  return {
-    isTeacherLoggedIn: !!state.teacher.id,
-  };
-};
+// const mapState = state => {
+//   return {
+//     isTeacherLoggedIn: !!state.teacher.id,
+//   };
+// };
 
-const mapDispatch = dispatch => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
-};
-export default withRouter(
-  connect(
-    mapState,
-    mapDispatch
-  )(Routes)
-);
+// const mapDispatch = dispatch => {
+//   return {
+//     loadInitialData() {
+//       dispatch(me());
+//     },
+//   };
+// };
+
+// export default withRouter(
+//   connect(
+//     mapState,
+//     mapDispatch
+//   )(Routes)
+// );
+
+export default Routes;
