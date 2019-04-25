6b034964109207bb5e5d3346362953d6c201d77d
Lindsay Strasser <lindsay.strasser@gmail.com>
2019-04-25 17:37:34 -0400

    comments out code for future use

--- client/components/teacher-homepage.js
+++ client/components/teacher-homepage.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 export const TeacherHome = props => {
-  const { email } = props;
+  // const { email } = props;
 
-  return props.email ? (
-    <div id="welcome-user">
-      <h4>Good Morning, {email}</h4>
-    </div>
-  ) : (
-    <div>Good Morning!!</div>
-  );
+  return <div>Good Morning!!</div>;
+  // props.email ? (
+  //   <div id="welcome-user">
+  //     <h4>Good Morning, {email}</h4>
+  //   </div>
+  // ) :
 };
 
-const mapState = state => {
-  return {
-    email: state.user.email,
-  };
-};
+// const mapState = state => {
+//   return {
+//     email: state.user.email,
+//   };
+// };
 
-export default connect(mapState)(TeacherHome);
+export default TeacherHome;
+
+// export default connect(mapState)(TeacherHome);
