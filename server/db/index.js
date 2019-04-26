f98bf9c795a8457a9beced579b601b4800d00835
Lindsay Strasser <lindsay.strasser@gmail.com>
2019-04-26 15:53:45 -0400

    adds associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,22 @@
 const db = require('./database');
 const Teacher = require('./Teacher');
 const Student = require('./Student');
+const StudentAnswer = require('./StudentAnswer');
+const TeacherMessage = require('./TeacherMessage');
 
 Student.belongsTo(Teacher);
 Teacher.hasMany(Student, { primaryKey: 'primaryKey' });
 
-module.exports = {
+StudentAnswer.belongsTo(Student);
+Student.hasMany(StudentAnswer);
+
+TeacherMessage.belongsTo(Teacher);
+Teacher.hasMany(TeacherMessage);
+
+Student.module.exports = {
   db,
   Teacher,
   Student,
+  StudentAnswer,
+  TeacherMessage,
 };
