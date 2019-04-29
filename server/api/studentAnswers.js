446c619a10aa756ce5ca45e85270cf48a641274f
Lindsay Strasser <lindsay.strasser@gmail.com>
2019-04-28 23:00:45 -0400

    adds student answer route

---
+++ server/api/studentAnswers.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { StudentAnswer } = require('../db');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const answers = await StudentAnswer.findAll();
+    res.json(answers);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const answer = await StudentAnswer.find({
+      where: { id: req.params.id },
+    });
+    res.json(answer);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { mood, comment } = req.body;
+    res.status(201);
+    res.json(
+      await StudentAnswer.create({
+        mood,
+        comment,
+      })
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- src/client/components/student-input-form.js
+++ src/client/components/student-input-form.js
@@ -39,67 +39,68 @@ class StudentInputForm extends Component {
           <div className="col-sm-12">
             <form onSubmit={this.handleSubmit}>
               <h2>How are you this morning?</h2>
-              <div className="radio">
-                <label>
-                  <input
-                    type="radio"
-                    name="excellent"
-                    value="excellent"
-                    checked={this.state.mood === 'excellent'}
-                    onChange={this.handleOptionChange}
-                  />
-                  Excellent
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input
-                    type="radio"
-                    name="mood"
-                    value="great"
-                    checked={this.state.mood === 'great'}
-                    onChange={this.handleOptionChange}
-                  />
-                  Great
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input
-                    type="radio"
-                    name="mood"
-                    value="fine"
-                    checked={this.state.mood === 'fine'}
-                    onChange={this.handleOptionChange}
-                  />
-                  Fine
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input
-                    type="radio"
-                    name="mood"
-                    value="bad"
-                    checked={this.state.mood === 'bad'}
-                    onChange={this.handleOptionChange}
-                  />
-                  Bad
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input
-                    type="radio"
-                    name="mood"
-                    value="terrible"
-                    checked={this.state.mood === 'terrible'}
-                    onChange={this.handleOptionChange}
-                  />
-                  Terrible
-                </label>
-              </div>
-              <div>
+              <div className="answer">
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="excellent"
+                      value="excellent"
+                      checked={this.state.mood === 'excellent'}
+                      onChange={this.handleOptionChange}
+                    />
+                    Excellent
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="mood"
+                      value="great"
+                      checked={this.state.mood === 'great'}
+                      onChange={this.handleOptionChange}
+                    />
+                    Great
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="mood"
+                      value="fine"
+                      checked={this.state.mood === 'fine'}
+                      onChange={this.handleOptionChange}
+                    />
+                    Fine
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="mood"
+                      value="bad"
+                      checked={this.state.mood === 'bad'}
+                      onChange={this.handleOptionChange}
+                    />
+                    Bad
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="mood"
+                      value="terrible"
+                      checked={this.state.mood === 'terrible'}
+                      onChange={this.handleOptionChange}
+                    />
+                    Terrible
+                  </label>
+                </div>
+                <div />
                 <h2>
                   Is there anything you would like to share with your teacher?
                 </h2>
